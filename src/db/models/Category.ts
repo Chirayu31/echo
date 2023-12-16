531e23c1ef78452c96fe3316b774a4353fd3d1e0
Chirayu <chirayupancholi17@gmail.com>
2023-12-16 19:26:37 +0530

    feat(db): category model design complete

---
+++ src/db/models/Category.ts
@@ -0,0 +1,24 @@
+import mongoose, { Document, Schema, model, Types, models } from 'mongoose';
+
+interface CategoryDocument extends Document {
+    userId: Types.ObjectId;
+    name: string;
+    type: 'income' | 'expense' | 'saving';
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const categorySchema = new Schema<CategoryDocument>(
+    {
+        userId: { type: Schema.Types.ObjectId, required: true },
+        name: { type: String, required: true },
+        type: { type: String, enum: ['income', 'expense', 'saving'], required: true },
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now },
+    },
+    { versionKey: false }
+);
+
+const CategoryModel = models?.Category as mongoose.Model<CategoryDocument> || model<CategoryDocument>('Category', categorySchema);
+
+export default CategoryModel;
